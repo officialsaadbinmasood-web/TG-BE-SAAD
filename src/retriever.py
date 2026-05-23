1bf05a37ca63f910d422948167b42b9359f3031c
officialsaadbinmasood-web <officialsaadbinmasood@gmail.com>
2026-05-23 23:53:10 +0500

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -10,7 +10,7 @@ class Retriever:
     def __init__(self, embedding_provider: EmbeddingProvider):
         self._embedder = embedding_provider
         client = chromadb.PersistentClient(path=str(CHROMA_PERSIST_DIR))
-        self._collection = client.get_collection(COLLECTION_NAME)
+        self._collection = client.get_or_create_collection(COLLECTION_NAME)
 
     async def retrieve(self, query: str, top_k: int = TOP_K) -> list[str]:
         query_embedding = await self._embedder.embed_query(query)
